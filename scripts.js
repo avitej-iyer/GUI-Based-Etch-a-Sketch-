b0d8dbb8ff10b0a587c6c0233fe77589c3d2b6fb
Avi_Ubuntu <iyeravitej2@gmail.com>
2023-01-04 21:17:20 -0500

    added further scripts for mouse actions

--- scripts.js
+++ scripts.js
@@ -6,6 +6,8 @@ let selected_col = default_col;
 let selected_mode = default_mode;
 let selected_size = default_size;
 
+let mouse_down = false;
+
 function set_color(new_color){
     selected_col = new_color;
 }
@@ -19,6 +21,10 @@ function set_size(new_size){
     selected_size = new_size;
 }
 
+function update_size(val){
+    size_val.innerHTML = '${val} x ${val}';
+}
+
 let color_picker_inp = document.getElementById('color_picker');
 let custom_color_button = document.getElementById('custom_button');
 let rainbow_button = document.getElementById('rainbow_button');
@@ -27,3 +33,21 @@ let clear_board_button = document.getElementById('clear_board_button');
 let size_val = document.getElementById('size_value');
 let size_inp = document.getElementById('size_slider');
 let drawing_board = document.getElementById('board');
+
+color_picker_inp.oninput = (inp) => set_color(inp.target.value);
+custom_color_button.onclick = () => set_mode('color');
+rainbow_button.onclick = () => set_mode('rainbow');
+eraser_button.onclick = () => set_mode('eraser');
+clear_board_button.ondblclick = () => clear_grid();
+size_inp.onmousemove = (inp) => update_size(inp.target.value);
+size_inp.onchange = (inp) => changeSize(e.target.value);
+
+document.body.onmousedown = () => (mouse_down = true);
+document.body.onmouseup = () => (mouse_down = false);
+
+function changeSize(val){
+    set_size(val);
+    update_size(val);
+    clear_grid();
+}
+
